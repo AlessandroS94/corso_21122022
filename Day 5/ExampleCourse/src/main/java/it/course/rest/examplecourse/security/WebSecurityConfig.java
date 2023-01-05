e2b5c96bbfa3092474a8026ce6865bfc1566f74c
AlessandroS94 <alessandro.sallese@student.univaq.it>
2023-01-05 09:22:43 +0100

    update

--- Day 5/ExampleCourse/src/main/java/it/course/rest/examplecourse/security/WebSecurityConfig.java
+++ Day 5/ExampleCourse/src/main/java/it/course/rest/examplecourse/security/WebSecurityConfig.java
@@ -40,10 +40,8 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
   @Bean
   public DaoAuthenticationProvider authenticationProvider() {
       DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-       
       authProvider.setUserDetailsService(userDetailsService);
       authProvider.setPasswordEncoder(passwordEncoder());
-   
       return authProvider;
   }
   
