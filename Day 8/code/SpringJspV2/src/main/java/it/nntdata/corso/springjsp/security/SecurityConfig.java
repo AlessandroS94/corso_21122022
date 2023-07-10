edc1560b32530f2911ede475518a97aef136b629
AlessandroS94 <alessandro.sallese@student.univaq.it>
2023-07-10 15:58:42 +0200

    Day 8
    + Update to Springboot 3.1.1

--- Day 8/code/SpringJspV2/src/main/java/it/nntdata/corso/springjsp/security/SecurityConfig.java
+++ Day 8/code/SpringJspV2/src/main/java/it/nntdata/corso/springjsp/security/SecurityConfig.java
@@ -9,38 +9,47 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-/*
 @Configuration
-public class SecurityConfiguration {
+class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails user = User.withUsername("user")
                 .password(passwordEncoder.encode("password"))
                 .roles("USER")
                 .build();
 
-        return new InMemoryUserDetailsManager(user);
+        UserDetails admin = User.withUsername("user2")
+                .password(passwordEncoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
     }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .requestMatchers("/")
-                .permitAll()
+        http
+                .csrf().disable()
+                .formLogin()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .clearAuthentication(true)
+                .logoutSuccessUrl("/login")
+                .deleteCookies()
+                .invalidateHttpSession(true)
                 .and()
                 .authorizeRequests()
-                .requestMatchers("/admin/**")
-                .hasRole("ADMIN")
-                .anyRequest()
-                .authenticated()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/").permitAll()
                 .and()
                 .httpBasic();
         return http.build();
     }
 }
 
- */
+
