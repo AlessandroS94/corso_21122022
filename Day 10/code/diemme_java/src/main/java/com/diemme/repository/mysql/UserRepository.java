be780b45ce37572fcd859eb43446edafdaef8440
AlessandroS94 <alessandro.sallese@student.univaq.it>
2023-07-17 01:06:57 +0200

    update

--- Day 10/code/diemme_java/src/main/java/com/diemme/repository/mysql/UserRepository.java
+++ Day 10/code/diemme_java/src/main/java/com/diemme/repository/mysql/UserRepository.java
@@ -15,7 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	User findByEmail(String email);
 
 	User findByUserName(String userName);
-
 	@Query("SELECT u FROM User u JOIN u.roles r WHERE r.role = :role")
 	Set<User> findUserByRole(@Param("role") String role);
 
